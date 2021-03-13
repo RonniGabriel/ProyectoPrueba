78b3f85c9278910e43ca064d723e1725c193bebd
RonnyGabriel <ronnigabrielg@gmail.com>
2021-03-13 19:39:39 +0100

    Primer cambio en METODOS

--- src/metodos.java
+++ src/metodos.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class metodos {
-
+ 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+String cadena1;
+String cadena2;
+
+cadena1	= "Perro";
+cadena2 = "Perro";
+
+if (cadena1==cadena2){
+	System.out.println ("CORRECTO");
+	
+}else {
+			System.out.println("INCORRECTO");
 }
+}
+}
+ 
+
